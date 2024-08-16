2ad613c597359efeb5b2632beb8f3d3f5c0b7275
sasha-corn <sashagricenko009@gmail.com>
2024-08-16 13:56:47 +0300

    homework 7.1

---
+++ lesson 7/hm 7.1.py
@@ -0,0 +1,12 @@
+
+def say_hi(name, age):
+    print(f"Hi. My name is {name} and I'm {age} years old")
+
+
+person1 = 'Vova'
+person2 = 'Alex'
+age_1 = 86
+age_2 = 15
+say_hi(person1, age_1)
+say_hi(person2, age_2)
+print('Bye')
