9d9eb3981be943342139ede0655604d9bc8f1062
sasha-corn <sashagricenko009@gmail.com>
2024-08-16 14:53:19 +0300

    homework 7.2

---
+++ lesson 7/hm 7.2.py
@@ -0,0 +1,13 @@
+
+def correct_sentence(text):
+    text += '.' if text[-1] != '.' else ''
+    new_text = text[0].upper() + text[1:]
+    return new_text
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('OK')
