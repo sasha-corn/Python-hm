f183a9d5cd187496639f4da3fd506f1ae17fc285
sasha-corn <sashagricenko009@gmail.com>
2024-08-19 17:13:50 +0300

    homework 9.1

---
+++ lesson 9/hm 9.1.py
@@ -0,0 +1,10 @@
+def popular_words(text, words):
+    text = text.lower()
+    word_count = {}
+    for word in words:
+        word_count.setdefault(word, text.count(word + ' '))
+
+    return word_count
+
+
+popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
