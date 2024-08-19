692ef50f3d1e68d1c171a251aec0ce3fe55d585d
sasha-corn <sashagricenko009@gmail.com>
2024-08-19 18:24:48 +0300

    homework 8.2

---
+++ lesson 8/hm 8.2.py
@@ -0,0 +1,15 @@
+import string
+
+
+def is_palindrome(text):
+    text = ''.join(char.lower() for char in text if char not in string.punctuation and char != ' ')
+    text2 = text[::-1]
+    return True if text == text2 else False
+
+
+# print(is_palindrome('A man, a plan, a canal: Panama'))
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
