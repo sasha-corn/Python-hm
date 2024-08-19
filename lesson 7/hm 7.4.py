2589097c256c62e7c74c0192b913553ad0711ddd
sasha-corn <sashagricenko009@gmail.com>
2024-08-19 15:44:28 +0300

    homework 7.3

--- lesson 7/hm 7.4.py
+++ lesson 7/hm 7.4.py
@@ -1,9 +1,7 @@
 
 def common_elements():
-    set_for3 = set()
-    set_for5 = set()
-    set_for3.update(char for char in range(101) if char % 3 == 0)
-    set_for5.update(char for char in range(101) if char % 5 == 0)
+    set_for3 = {char for char in range(101) if char % 3 == 0}
+    set_for5 = {char for char in range(101) if char % 5 == 0}
     return set_for3.intersection(set_for5)
 
 
