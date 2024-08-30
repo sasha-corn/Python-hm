59909b74ffdab19cb0a6caea54b20230ab11d523
sasha-corn <sashagricenko009@gmail.com>
2024-08-30 18:17:03 +0300

    homework 11.2

---
+++ lesson 11/hm 11.2.py
@@ -0,0 +1,23 @@
+from inspect import isgenerator
+
+
+def cube_number(num):
+    num **= 3
+    return num
+
+
+def generate_cube_numbers(end):
+    for num in range(2, end):
+        cub = cube_number(num)
+        if cub <= end:
+            yield cub
+        else:
+            return
+
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) is True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print('ok')
