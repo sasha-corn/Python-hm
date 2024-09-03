84d74609db4335f3f3773270cf533456de391d24
sasha-corn <sashagricenko009@gmail.com>
2024-09-03 22:11:41 +0300

    homework 12.2

--- lesson 12/hm 12.2.py
+++ lesson 12/hm 12.2.py
@@ -7,8 +7,7 @@ class Product:
         self.dimensions = dimensions
 
     def __str__(self):
-        return f'''{self.name}
-        Bартість: {self.price}грн, колір: {self.description}, розмір: {self.dimensions}'''
+        return f'{self.name}, вартість: {self.price}'
 
 
 class User:
@@ -19,9 +18,7 @@ class User:
         self.number = number
 
     def __str__(self):
-        return f'''Користувач:
-    {self.name} {self.surname}
-    тел.:{self.number}'''
+        return f'{self.name} {self.surname}'
 
 
 class Order:
@@ -35,15 +32,15 @@ class Order:
         self.product[item] = cnt
 
     def get_total(self):
+        self.total = 0
         for cost, num in self.product.items():
             self.total += cost.price * num
         return self.total
 
     def __str__(self):
-        result = f'''{self.user}
-Список покупок:'''
+        result = f'Користувач: {self.user}\nСписок покупок:'
         for item, cnt in self.product.items():
-            result += f'\n\t{item.name}: {cnt} шт.'
+            result += f'\n{item.name}: {cnt} шт.'
         return result
 
 
@@ -58,5 +55,13 @@ cart = Order(person_one)
 cart.add_product(shirt, 1)
 cart.add_product(pants, 2)
 
+print(cart)
+print(f'Ціна: {cart.get_total()} грн')
+
+
+assert isinstance(cart.user, User) is True
+assert cart.get_total() == 1830
+assert cart.get_total() == 1830
+cart.add_product(pants, 10)
 print(cart)
 print(f'Ціна: {cart.get_total()} грн')
